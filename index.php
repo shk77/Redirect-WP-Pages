ff3d61278a8aca516fd61b68f25ebb5c9ffc1336
shk77 <bhandarishekhar0@gmail.com>
2015-05-31 14:32:46 +0545

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WP Login Logout Menu
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A Plugin To Enable Pages To have ability to show/hide to Login and No Login Users
+ * Version: The plugin's version number. Example: 1.0.0
+ * Author: Shekhar Bhandari
+ * Author URI: http://www.shekharbhandari.com.np
+ * Text Domain: wp-login-logout-menu
+ * License: Example: GPL2
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+include_once(dirname(__FILE__).'/settings.php');
+
+
+
+
+
