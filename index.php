5d5305a84496c949d4500f76929e058d1bde960c
shk77 <bhandarishekhar0@gmail.com>
2015-05-31 14:38:58 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Plugin Name: WP Login Logout Menu
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Plugin Name: Restrict WP Pages With Redirect
  * Description: A Plugin To Enable Pages To have ability to show/hide to Login and No Login Users
- * Version: The plugin's version number. Example: 1.0.0
+ * Version: 1.0.0
  * Author: Shekhar Bhandari
  * Author URI: http://www.shekharbhandari.com.np
- * Text Domain: wp-login-logout-menu
+ * Text Domain: restrict-wp-pages-with-redirect
  * License: Example: GPL2
  */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
