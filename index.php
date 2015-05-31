283813a9c5f239b1906a0458ed021dcabfbccbbb
shk77 <bhandarishekhar0@gmail.com>
2015-05-31 14:39:53 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Restrict WP Pages With Redirect
- * Description: A Plugin To Enable Pages To have ability to show/hide to Login and No Login Users
+ * Description: A Plugin To Enable Pages To have ability to show/hide to Login and No Login Users and redirect them.
  * Version: 1.0.0
  * Author: Shekhar Bhandari
  * Author URI: http://www.shekharbhandari.com.np
